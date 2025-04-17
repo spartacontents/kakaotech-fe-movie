6feb6873ce85565c504d863df51ee3203ac798f4
spartacontents <contents@teamsparta.co>
2025-04-17 15:31:09 +0900

    4. 검색 기능 구현하기

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const API_KEY = "발급한 API KEY를 입력하세요.";
+const API_KEY = "c16ac3801e10111f7251235a3f0cf44b";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 function displayMovies(movies) {
+  const movieListRef = document.getElementById("movie-list");
+  movieListRef.innerHTML = "";
   movies.forEach(element => {
     const {title, poster_path, vote_average} = element;
     const movieCard = document.createElement("div");
@@ -11,7 +13,7 @@ function displayMovies(movies) {
       <h3>${title}</h3>
       <p>${vote_average}</p>
     `
-    document.getElementById("movie-list").appendChild(movieCard)
+    movieListRef.appendChild(movieCard);
   });
 }
 
@@ -20,11 +22,25 @@ function fetchPopularMovies() {
     .then((res) => res.json())
     .then((res) => {
       displayMovies(res.results)
-
     })
     .catch((err) => console.error(err));
 }
 
+function searchMovies(keyword) {
+  fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${keyword}`)
+    .then((res) => res.json())
+    .then((res) => {
+      displayMovies(res.results)
+    })
+    .catch((err) => console.error(err));
+}
+
+
 document.addEventListener("DOMContentLoaded", function () {
   fetchPopularMovies();
+
+  document.getElementById("search-btn").addEventListener("click", () => {
+    const keyword = document.getElementById("search-input").value;
+    searchMovies(keyword);
+  });
 });

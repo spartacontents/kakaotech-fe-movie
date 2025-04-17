6c3755f566a11eb46d2e8c922af9ee221add69e6
spartacontents <contents@teamsparta.co>
2025-04-17 14:15:57 +0900

    2. API로부터 데이터 받아오기

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hihi");
+const API_KEY = "발급한 API KEY를 입력하세요.";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+function fetchPopularMovies() {
+  fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetchPopularMovies();
+});
